5eb8925fc2c9c5f9e3b48c53145f87c48de613cb
Tim Oladoyinbo <dev@digitalkraaft.com>
2020-07-07 12:19:23 +0100

    intial commit

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -9,6 +9,6 @@ class TestHelper
     public static function setup()
     {
         Flutterwave::$apiBase = 'https://api.flutterwave.com/v3';
-        Flutterwave::setApiKey('sk_abcd12345');
+        Flutterwave::setApiKey('FLWSECK_TEST-abcd12345');
     }
 }
